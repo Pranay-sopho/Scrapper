47df506d44e8797025b8151b37810a17d9200edf
Pranay Kotkar <kotkar.pranay04@gmail.com>
2017-04-18 17:05:07 +0000

    Url Validator Implemented

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -11,15 +11,22 @@
 $(document).ready(function() {
     $("#url").on('input', emptydata());
     $(".url-form").submit(function(e) {
-        $("#loader").css('display', 'inline-block');
         e.preventDefault();
         var url = $("#url").val();
         var regex = /(http:\/\/www\.shiksha\.com\/b-tech\/colleges\/b-tech-colleges-[^\d]*[^\d\-])(?=-\d.*)?/i;
         var urld = regex.exec(url);
-        var urlr = urld[1];
-        url = urlr;
-        var page = 1;
-        getdata(urlr, url, page);
+        if (urld) {
+            $("#loader").css('display', 'inline-block');
+            var urlr = urld[1];
+            url = urlr;
+            var page = 1;
+            getdata(urlr, url, page);
+        }
+        else {
+            $("#url").val('');
+            $('#myModal').modal('show');
+            return false;
+        }
     });
 });
 
--- views/url.php
+++ views/url.php
@@ -4,4 +4,25 @@
         <img id="loader" class="col-lg-2" src="loader.gif" alt="Scrapping Data ...">
         <a id="result" href="display.php">Show data</a>
     </form>
+    <div class="container">
+        <div class="modal fade" id="myModal" role="dialog">
+            <div class="modal-dialog">
+            
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Error!!!</h4>
+                    </div>
+                    <div class="modal-body">
+                        <p>Enter Valid Url</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+              
+            </div>
+        </div>
+    </div>
 </div>
